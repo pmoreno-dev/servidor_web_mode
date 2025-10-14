99b5db9fc868226ba6851d058a3e32c9a6c8da08
pmoreno-dev <pablo.morgv@gmail.com>
2025-10-14 16:31:17 +0000

    fix

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const app = require('./app')
 const  {PORT} = process.env
 
 app.listen(PORT, () => 
-    console.log('Server is running in port ${Port}')
+    console.log(`Server is running in port ${PORT}`)
 )
 
 module.exports = app
